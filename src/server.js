4f95e0d8f7f80eb511fa5f97dbe1c9aec9f1aa64
marrcelosantana <marcelosantana@alu.ufc.br>
2023-03-07 20:28:42 -0300

    [Fundamentos-Node] - Updates

--- src/server.js
+++ src/server.js
@@ -1,7 +1,40 @@
 import http from "http";
 
-const server = http.createServer((request, response) => {
-  return response.end("Hello World");
+const users = [];
+
+const server = http.createServer(async (request, response) => {
+  const { method, url } = request;
+
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    request.body = null;
+  }
+
+  if (method === "GET" && url === "/users") {
+    return response
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    const { name, email } = request.body;
+
+    users.push({
+      id: 1,
+      name,
+      email,
+    });
+    return response.writeHead(201).end();
+  }
+
+  return response.writeHead(404).end();
 });
 
 server.listen(3333);
